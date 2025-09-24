6f5450ef9f33ac2a3c344b0e7e9db874ab35cbb6
trmore-1712 <TRMORE_b24@ce.vjti.ac.in>
2025-09-24 23:45:58 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from planner1 import planner
+from planner import planner
 from executor import Executor
 from supervisor import Supervisor
 
@@ -7,33 +7,37 @@ def main():
     
     user_input=input("Enter your task: ")
     messages.append({"role": "user","content": user_input})
-    print(messages,"\n\n")
+    
     while True:
         
         planner_output=planner.generate_reply(messages)
         messages.append(planner_output)
         print("----Planner-----")
         print(planner_output)
-        
-        print(messages,"\n\n")
+        # print(messages)
         
         executor_output = Executor.generate_reply(messages)
         messages.append(executor_output)
         print("----Executor-----")
         print(executor_output)
-        print(messages,"\n\n")
         
         supervisor_feedback = Supervisor.generate_reply(messages)
         messages.append(supervisor_feedback)
         print("----Supervisor-----")
         print(supervisor_feedback)
-        print(messages,"\n\n")
         
         if  supervisor_feedback["content"]["is_terminate"]:
             break
-
-# FInd cheapest flight from mumbai to delhi for 2 adults on 5th september
-# find the most starred repository for react on github
+    
+    final_ans=""
+    for i in range(-1,-len(messages)-1,-1):
+      if messages[i]["role"]=="Executor":
+        extracted_ans=messages[i]["content"]["extract"]
+        break
+    for ans in extracted_ans:
+       final_ans+=ans["name"]+" "
+    print("Desired answer:",final_ans)
+# FInd cheapest flight from mumbai to delhi for 2 adults on 5th september from ixigo site
 
 if __name__ == "__main__":
     main()
