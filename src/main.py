dcc6c727e1b3c96d09d23bf70c51966eddf1eb6d
trmore-1712 <TRMORE_b24@ce.vjti.ac.in>
2025-09-28 19:31:57 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from planner import planner
-from executor import executor
-from supervisor import supervisor
+from executor import Executor
+from supervisor import Supervisor
 
 def main():
     messages=[]
@@ -9,29 +9,35 @@ def main():
     messages.append({"role": "user","content": user_input})
     
     while True:
-                    
+        
         planner_output=planner.generate_reply(messages)
         messages.append(planner_output)
         print("----Planner-----")
         print(planner_output)
-        print(messages,"\n\n")
+        # print(messages)
         
         executor_output = Executor.generate_reply(messages)
         messages.append(executor_output)
         print("----Executor-----")
         print(executor_output)
         
-        supervisor_feedback = supervisor.generate_reply(messages=messages, sender=supervisor)
+        supervisor_feedback = Supervisor.generate_reply(messages)
         messages.append(supervisor_feedback)
         print("----Supervisor-----")
         print(supervisor_feedback)
-        print(messages,"\n\n")
-        #if len(messages)>=6:
-        #    del messages[-4]
+        
         if  supervisor_feedback["content"]["is_terminate"]:
             break
-
-
-
+    
+    final_ans=""
+    for i in range(-1,-len(messages)-1,-1):
+      if messages[i]["role"]=="Executor":
+        extracted_ans=messages[i]["content"]["extract"]
+        break
+    for ans in extracted_ans:
+       final_ans+=ans["name"]+" "
+    print("Desired answer:",final_ans)
+# FInd cheapest flight from mumbai to delhi for 2 adults on 5th september from ixigo site
 if __name__ == "__main__":
     main()
+
