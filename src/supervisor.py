abe1d9cba978eb05c8c906d74dbab4afc602585b
trmore-1712 <TRMORE_b24@ce.vjti.ac.in>
2025-10-05 23:36:25 +0530

    Update supervisor.py

--- src/supervisor.py
+++ src/supervisor.py
@@ -2,6 +2,17 @@ import base64
 import json
 from openai import OpenAI
 from autogen import AssistantAgent
+from dotenv import load_dotenv
+import os,json
+import google.generativeai as genai
+from PIL import Image
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("API key not found")
+
+genai.configure(api_key=api_key)
 
 sys_prompt="""
 You are a reasoning supervisor agent. 
@@ -37,8 +48,9 @@ You will always receive the input in this JSON-like format:
    - Reason about the action ,after performing what it can led to.
    - Not always the changes predicted will be as it is dont be very rigid with the prediction,some slight changes will work too "but not next to nothing" ,give success as false only if any major change to happen isnt there and you are confident about it.   
 3. Give reasoning step by step.  
-4. Conclude whether the task succeeded or failed. 
-6. Reason about the main user task if completed or not with provided list of sub steps. 
+4. Conclude whether the task succeeded or failed.
+5. Task isnt complete on getting to the search results page so don't mark it as terminate unnecessarily as it terminates when top 5 links are visited and result is aggregated. 
+6. Reason about the main user task if completed or not with provided list of sub steps.
 
 ## Output Format
 Always respond in the following JSON format:
@@ -62,27 +74,38 @@ Don't output anything else than this format.
 - Stick to the output format strictly dont output anything extra than the json output format explicitly mentioned.
 
 """
-def encode_image(path: str, mime: str = "png") -> str:
-    with open(path, "rb") as f:
-        b64 = base64.b64encode(f.read()).decode("utf-8")
-    return f"data:image/{mime};base64,{b64}"
+# def encode_image(path: str, mime: str = "png") -> str:
+#     with open(path, "rb") as f:
+#         b64 = base64.b64encode(f.read()).decode("utf-8")
+#     return f"data:image/{mime};base64,{b64}"
 
-def analyze_image(image_path, prompt,client,model, mime: str = "png"):
-    data_url = encode_image(image_path, mime)
-    completion = client.chat.completions.create(
-        model=model,
-        temperature=0.5,
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": prompt},
-                    {"type": "image_url", "image_url": {"url": data_url}}
-                ]
-            }
-        ]
+def analyze_image(image_path, prompt):
+    # data_url = encode_image(image_path, mime)
+    # completion = client.chat.completions.create(
+    #     model=model,
+    #     temperature=0.5,
+    #     messages=[
+    #         {
+    #             "role": "user",
+    #             "content": [
+    #                 {"type": "text", "text": prompt},
+    #                 {"type": "image_url", "image_url": {"url": data_url}}
+    #             ]
+    #         }
+    #     ]
+    # )
+    # return completion.choices[0].message.content
+    model = genai.GenerativeModel(
+        model_name='gemini-2.5-flash-lite',
+        system_instruction=sys_prompt
     )
-    return completion.choices[0].message.content
+    prompt_parts = [
+        prompt,
+        image_path
+    ]
+
+    response = model.generate_content(prompt_parts)
+    return response.text
 
 def supervisor_generate(agent, messages, sender, config):
     for i in range(-1,-len(messages)-1,-1):
@@ -128,29 +151,40 @@ def supervisor_generate(agent, messages, sender, config):
     
     all_actions.reverse()
 
-    v_cfg = config["config_list"][0]
+    # v_cfg = config["config_list"][0]
     t_cfg = config["config_list"][1]
 
-    v_client = OpenAI(
-            base_url=v_cfg["base_url"],   
-            api_key=v_cfg["api_key"],
-        )
+    # v_client = OpenAI(
+    #         base_url=v_cfg["base_url"],   
+    #         api_key=v_cfg["api_key"],
+    #     )
     t_client = OpenAI(
             base_url=t_cfg["base_url"],   
             api_key=t_cfg["api_key"],
         )
     
-    v_model=v_cfg["model"]
+    # v_model=v_cfg["model"]
     t_model=t_cfg["model"]
 
-    image1 = r"Sera--Smart-Search-Assistant/pre_ss.png"
-    image2 = r"Sera--Smart-Search-Assistant/post_ss.png"
+    # image1 = r"C:\Users\tanmay\OneDrive\Desktop\Autogen\pre_ss.png"
+    # image2 = r"C:\Users\tanmay\OneDrive\Desktop\Autogen\post_ss.png"
 
+    # prompt1 = "List all interactive elements visible on this webpage screenshot with the possible actions after interacting with them can led to."
+    # prompt2 = "Summarize this page what it is about and what all element it contains"
+    pre_image = "pre_ss.png"
+    post_image="post_ss.png"
+    
     prompt1 = "List all interactive elements visible on this webpage screenshot with the possible actions after interacting with them can led to."
     prompt2 = "Summarize this page what it is about and what all element it contains"
 
-    pre_state=analyze_image(image1, prompt1,v_client,v_model, mime="png")
-    post_state=analyze_image(image2, prompt2,v_client,v_model, mime="png")
+    img1 = Image.open(pre_image)
+    img2 = Image.open(post_image)
+
+
+    pre_state=analyze_image(img1, prompt1)
+    print("pre_state")
+    post_state=analyze_image(img2, prompt2)
+    print("post_state")
 
     user_input = {
         "action": sub_task,
@@ -168,7 +202,8 @@ def supervisor_generate(agent, messages, sender, config):
         ]
     )
     feedback=completion.choices[0].message.content
-    dict_feedback=json.loads(feedback)
+    dict_feedback = json.loads(feedback.replace('\\', '\\\\'))
+
     messages.append({
         "role": agent.name,
         "content": dict_feedback
@@ -177,7 +212,7 @@ def supervisor_generate(agent, messages, sender, config):
         "role": agent.name,
         "content": dict_feedback
         }
-import os
+
 Supervisor = AssistantAgent(
     name="Supervisor",
     llm_config={
@@ -185,12 +220,12 @@ Supervisor = AssistantAgent(
             {
                 "model": "google/gemma-3-27b-it:free",
                 "base_url": "https://openrouter.ai/api/v1",
-                "api_key": os.environ.get("ROUT")
+                "api_key": "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"
             },
             {
                 "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
                 "base_url": "https://openrouter.ai/api/v1",
-                "api_key": os.environ.get("ROUT")
+                "api_key": "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"
             }
         ]
     }
